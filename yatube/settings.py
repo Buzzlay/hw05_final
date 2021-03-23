56bd9c0bdd107b077e9555fe1bfd9069abfdaeac
Buzzlay <dimabuslaev@gmail.com>
2021-03-23 16:15:37 +0500

    media

--- yatube/settings.py
+++ yatube/settings.py
@@ -107,7 +107,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, './posts/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, './media')
